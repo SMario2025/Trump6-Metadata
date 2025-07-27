7900d093a1e33af26eb9d6962fa7e32cd4af3adc
SMario2025 <pascal-merzenich@web.de>
2025-07-27 16:33:45 +0200

    Create VERIFICATION.m

---
+++ VERIFICATION.m
@@ -0,0 +1,11 @@
+# VERIFICATION
+
+Ich bestätige hiermit, dass ich der Besitzer der folgenden Solana Mint-Adresse bin:
+
+`2gdbFuUfCDdhSCbqbnkw4ZWifCB8JLVVgJmBgUH5V8kK`
+
+Dies ist das **offizielle GitHub-Metadaten-Repository** für den Token **Trump6**.
+
+Signiert:  
+@SMario2025
+
